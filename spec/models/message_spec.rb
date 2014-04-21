ce7f22b9288a31a7031750b7e464cee46a7e32ba
Brentwood Reid & Maigen Skjelstad <student@epicodus.com>
2014-04-21 12:38:23 -0700

    Add validation to Message model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe Message, :vcr => true do
+
+  it { should validate_presence_of :to }
+  it { should validate_presence_of :from }
+  it { should validate_presence_of :body }
+
   it "doesn't save the message if twilio gives an error" do
     message = Message.new(body: 'hi', to: '1111111', from: '5039256254')
     message.save.should be_false
