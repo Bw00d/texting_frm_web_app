bb73f644009efee50ca98025d13ab24cd3a94469
Brentwood Reid & Maigen Skjelstad <student@epicodus.com>
2014-04-21 16:48:01 -0700

    add a header to layout

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
-describe 'home page' do
+feature 'sign up' do
+  before  { visit new_user_registration_path }
 
+  it 'should let a user sign up with valid information' do
+    fill_in 'Email', with: "user@example.com"
+    fill_in 'Password', with: "foobarbaz"
+    fill_in 'Password confirmation', with: "foobarbaz"
+    click_button "Sign up"
+    expect(page).to have_content ("Welcome! You have signed up successfully.")
+  end
+
+  it 'should not create a new user if password it too short' do
+    fill_in 'Email', with: "user@example.com"
+    fill_in 'Password', with: "foobar"
+    fill_in 'Password confirmation', with: "foobar"
+    click_button "Sign up"
+    expect(page).to have_content ("Password is too short")
+  end
+  it 'should not create a new user if enail is invalid' do
+    fill_in 'Email', with: "user.example.com"
+    fill_in 'Password', with: "foobarbaz"
+    fill_in 'Password confirmation', with: "foobarbaz"
+    click_button "Sign up"
+    expect(page).to have_content ("Email is invalid")
+  end
 end
