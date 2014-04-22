e42c8c81bdc57687058807ff76a98594d181fb7b
Brentwood Reid & Maigen Skjelstad <student@epicodus.com>
2014-04-22 16:01:00 -0700

    Add new contact form to new contacts page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,19 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.create(contact_params)
+    if @contact.save
+      redirect_to new_message_path, notice: "Contact added."
+    else
+      redirect_to :back, alert: "You messed something up. Try again."
+    end
+  end
+
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :number, :user_id)
+    end
 end
