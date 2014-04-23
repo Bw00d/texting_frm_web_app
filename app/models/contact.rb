9eb85e7901c48eef657fc081d7de7d4d6157eda8
Brentwood Reid & Maigen Skjelstad <student@epicodus.com>
2014-04-22 17:04:51 -0700

    Add is_unique method to Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,16 @@
 class Contact < ActiveRecord::Base
   validates :name, presence: true
-  validates :number, presence: true
+  validates :number, presence: true,
+                     uniqueness: true
   belongs_to :user
+
+  def is_unique(new_number)
+    Contact.all.each do |number|
+      if number == new_number
+        return false
+      else
+        true
+      end
+    end
+  end
 end
